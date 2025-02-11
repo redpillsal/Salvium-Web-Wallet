39ff102e5e3bdb1f841fe7997e2ec5971c46fd5c
redpillsal <redpillsal@proton.me>
2025-02-11 09:59:52 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
 import { HashRouter as Router } from "react-router-dom";
+import SalviumWallet from "./SalviumWallet";
+import "./styles.css";
 
 ReactDOM.render(
   <Router>
-    <App />
+    <SalviumWallet />
   </Router>,
   document.getElementById("root")
-);
-
-
-import React from "react";
-import ReactDOM from "react-dom/client"; // Use the new createRoot API
-import "./index.css";
-import App from "./App";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
 );
